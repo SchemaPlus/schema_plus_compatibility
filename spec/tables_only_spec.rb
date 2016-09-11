59e11853e4422b2a66707e4ee3ba517e50821584
ronen barzel <ronen@barzel.org>
2016-09-11 08:45:50 +0100

    Don’t use SchemaMonkey module in spec
    It’s neat & clever to define a SchemaMonkey client to add support methods.  But I think we shouldn’t:  We’re testing additions & modifications to AR, we shouldn’t introduce additional ones as part of the testing.
    
    Also, while the helper was required and registered in this file, the effect is actually global, which could be confusing.

--- spec/tables_only_spec.rb
+++ spec/tables_only_spec.rb
@@ -1,27 +1,33 @@
 # encoding: utf-8
 require 'spec_helper'
-require_relative 'support/views'
-
-SchemaMonkey.register ViewSupport
 
 describe "tables_only" do
   let(:connection) { ActiveRecord::Base.connection }
+
+  def drop_dummy_view(v)
+    connection.execute "DROP VIEW IF EXISTS #{v}"
+  end
+
+  def create_dummy_view(v, t)
+    drop_dummy_view v
+    connection.execute "CREATE VIEW #{v} AS SELECT * FROM #{t}"
+  end
 
   around(:each) do |example|
     begin
       connection.tables_only.each do |table|
         connection.drop_table table, force: :cascade
       end
-      connection.create_table :t1, force: true
-      connection.create_table :t2, force: true
-      connection.create_dummy_view :v1
-      connection.create_dummy_view :v2
+      connection.create_table :t1
+      connection.create_table :t2
+      create_dummy_view :v1, :t1
+      create_dummy_view :v2, :t2
       example.run
     ensure
+      drop_dummy_view :v1
+      drop_dummy_view :v2
       connection.drop_table :t1, if_exists: true
       connection.drop_table :t2, if_exists: true
-      connection.drop_dummy_view :v1
-      connection.drop_dummy_view :v2
     end
   end
 
@@ -45,4 +51,6 @@ describe "tables_only" do
     # user_tables_only shouldn't return ar_internal_metadata
     expect(connection.user_tables_only).to match_array %w[t1 t2]
   end
+
+
 end
