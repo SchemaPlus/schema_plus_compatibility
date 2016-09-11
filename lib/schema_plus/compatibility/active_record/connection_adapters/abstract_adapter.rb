f56ce9f73a5c9df8ed247859f443f76d1774380f
ronen barzel <ronen@barzel.org>
2016-09-11 07:48:02 +0100

    Remove support for non-supported DBs

--- lib/schema_plus/compatibility/active_record/connection_adapters/abstract_adapter.rb
+++ lib/schema_plus/compatibility/active_record/connection_adapters/abstract_adapter.rb
@@ -7,22 +7,6 @@ module SchemaPlus::Compatibility
           t.delete "ar_internal_metadata"
           t
         end
-
-        def tables_only
-          # In the future (AR 5.1?) tables may really return just tables instead of data
-          # sources, and the deprecation warning will be removed. We would
-          # have to update the method then.
-          #
-          # For consistency's sake, this method is overridden in all standard adapters
-          # (SQLite3, MySQL and Postgres) with a proper tables-only implementation, so you're
-          # really going to get ONLY tables and no warning for these 3 databases across all
-          # current and future versions of AR.
-          # For non-supported DBs, try our best by trying first to subtract #views from #data_sources
-          # but we have to return the plain old #tables result in AR4.2.
-          all_sources = (try :data_sources) || tables
-          views_only = (try :views) || []
-          all_sources - views_only
-        end
       end
     end
   end
--- spec/tables_only_spec.rb
+++ spec/tables_only_spec.rb
@@ -4,7 +4,27 @@ require_relative 'support/views'
 
 SchemaMonkey.register ViewSupport
 
-RSpec.shared_examples 'tables_only_tests' do
+describe "tables_only" do
+  let(:connection) { ActiveRecord::Base.connection }
+
+  around(:each) do |example|
+    begin
+      connection.tables_only.each do |table|
+        connection.drop_table table, force: :cascade
+      end
+      connection.create_table :t1, force: true
+      connection.create_table :t2, force: true
+      connection.create_dummy_view :v1
+      connection.create_dummy_view :v2
+      example.run
+    ensure
+      connection.drop_table :t1, if_exists: true
+      connection.drop_table :t2, if_exists: true
+      connection.drop_dummy_view :v1
+      connection.drop_dummy_view :v2
+    end
+  end
+
   it "does not cause deprecation" do
     expect(ActiveSupport::Deprecation).not_to receive(:warn)
     connection.tables_only
@@ -26,94 +46,3 @@ RSpec.shared_examples 'tables_only_tests' do
     expect(connection.user_tables_only).to match_array %w[t1 t2]
   end
 end
-
-
-describe ActiveRecord::ConnectionAdapters::AbstractAdapter do
-  context "tables_only" do
-    let(:connection) { ActiveRecord::Base.connection }
-
-    around(:each) do |example|
-      begin
-        connection.tables_only.each do |table|
-          connection.drop_table table, force: :cascade
-        end
-        connection.create_table :t1, force: true
-        connection.create_table :t2, force: true
-        connection.create_dummy_view :v1
-        connection.create_dummy_view :v2
-        example.run
-      ensure
-        connection.drop_table :t1, if_exists: true
-        connection.drop_table :t2, if_exists: true
-        connection.drop_dummy_view :v1
-        connection.drop_dummy_view :v2
-      end
-    end
-
-    include_examples 'tables_only_tests'
-  end
-
-  context "tables_only with AR5-style custom adapter" do
-    class AR5CustomAdapter < ActiveRecord::ConnectionAdapters::AbstractAdapter
-      def initialize
-        super nil
-      end
-
-      def tables
-        raise NotImplementedError
-      end
-
-      def data_sources
-        %w[t1 t2 v1 v2]
-      end
-
-      def views
-        %w[v1 v2]
-      end
-    end
-
-    let(:connection) { AR5CustomAdapter.new }
-
-    include_examples 'tables_only_tests'
-  end
-
-  if ActiveRecord::VERSION::MAJOR < 5
-    context "tables_only with AR4.2 custom adapter with views" do
-      class AR4CustomAdapterWithViews < ActiveRecord::ConnectionAdapters::AbstractAdapter
-        def initialize
-          super nil
-        end
-
-        def tables
-          %w[t1 t2 v1 v2]
-        end
-
-        def views
-          %w[v1 v2]
-        end
-      end
-
-      let(:connection) { AR4CustomAdapterWithViews.new }
-
-      include_examples 'tables_only_tests'
-    end
-
-    context "tables_only with AR4.2 custom adapter" do
-      class AR4CustomAdapter < ActiveRecord::ConnectionAdapters::AbstractAdapter
-        def initialize
-          super nil
-        end
-
-        def tables
-          # Only consistent if adapter doesn't list views even though it has no notion of them.
-          %w[t1 t2]
-        end
-      end
-
-      let(:connection) { AR4CustomAdapter.new }
-
-      include_examples 'tables_only_tests'
-    end
-  end
-end
-
