9eca9bbbae3e350985cbc65a66f2f9e8e809732f
Boaz Yaniv <boazyan@gmail.com>
2016-10-20 09:57:02 +0900

    Added _pg_relations_sql

--- lib/schema_plus/compatibility/active_record/connection_adapters/postgresql_adapter.rb
+++ lib/schema_plus/compatibility/active_record/connection_adapters/postgresql_adapter.rb
@@ -17,17 +17,25 @@ module SchemaPlus::Compatibility
           sql
         end
 
+        def _pg_relations(rel_types, extra_sql = nil)
+          sql = _pg_relations_sql(rel_types)
+          if extra_sql
+            sql << "\n"
+            sql << extra_sql
+          end
+          select_values(sql, 'SCHEMA')
+        end
+
         # This private method tries the model the way ActiveRecord queries PostgreSQL relations, but allow for modifications
         # by middleware.
-        def _pg_relations(rel_types, extra_sql = '')
+        def _pg_relations_sql(rel_types)
           rel_type_query = rel_types.map{|t| "'#{t}'"}.join(',')
-          select_values(<<-SQL, 'SCHEMA')
+          <<-SQL
             SELECT c.relname
             FROM pg_class c
             LEFT JOIN pg_namespace n ON n.oid = c.relnamespace
             WHERE c.relkind IN (#{rel_type_query})
             AND n.nspname = ANY (current_schemas(false))
-            #{extra_sql}
           SQL
         end
       end
