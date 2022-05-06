39eee71774b5f436ffa6cfff0d916ec592f015b0
Edward Rudd <urkle@outoforder.cc>
2022-05-06 14:56:53 -0400

    add frozen string literal comments

--- lib/schema_plus/compatibility.rb
+++ lib/schema_plus/compatibility.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'schema_monkey'
 
 require_relative 'compatibility/version'
--- lib/schema_plus/compatibility/version.rb
+++ lib/schema_plus/compatibility/version.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module SchemaPlus
   module Compatibility
     VERSION = "1.0.0"
